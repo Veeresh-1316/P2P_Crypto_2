ef5ac75dd8fa2211f4731ec384f731fbb67956cc
Veeresh-1316 <vrushabhbp@gmail.com>
2024-04-21 17:42:35 +0530

    Update solidity.sol

--- solidity.sol
+++ solidity.sol
@@ -5,7 +5,7 @@ contract FakeNewsDApp {
     // Struct to store news items
     struct NewsItem {
         address uploader;
-        string contentHash;
+        string contenturl;
         uint256 timestamp;
         string category;
 
@@ -17,18 +17,18 @@ contract FakeNewsDApp {
     string[] identities;
 
     // Mapping to store fact-checker weights, reputations and other info
-    address[] private factCheckers;
-    mapping(address => mapping(string => uint256)) public weights;
-    mapping(address => uint) correct_votes;
-    mapping(address => uint) incorrect_votes;
-    mapping(address => uint256) reputations;
-    mapping(address => uint) public probation;
+    address[] factCheckers;
+    mapping(address => mapping(string => uint256)) weights;         // weights for votes [every category separate]
+    mapping(address => uint) correct_votes;                         // total correct votes
+    mapping(address => uint) incorrect_votes;                       // total incorrect votes
+    mapping(address => uint256) reputations;                        // reputation score as weightage for rewards re-distribution
+    mapping(address => uint) probation;                             // probabtion period ratio of weight
 
-    // Mapping to store news items, if voting period active
-    mapping(string => NewsItem) public newsItems;
-    mapping(string => bool) public voting;
+    // Mapping to store news items & if voting period active
+    mapping(string => NewsItem) newsItems;
+    mapping(string => bool) voting;
 
-    // list of categories
+    // list of news' categories
     string[] categories;
 
     // DEPOSIT FOR VOTING
@@ -36,6 +36,8 @@ contract FakeNewsDApp {
 
     // Event emitted when a new news item is uploaded
     event NewsItemUploaded(NewsItem indexed newItem);
+
+
 
     // Modifier to restrict function access to registered fact-checkers
     modifier onlyFactChecker() {
@@ -81,49 +83,49 @@ contract FakeNewsDApp {
     }
 
     // Function to upload a news item
-    function uploadNewsItem(string memory _contentHash, string _category) onlyFactChecker {
-        // bytes32 itemHash = keccak256(abi.encodePacked(_contentHash, block.timestamp, msg.sender));
-        string itemHash = _contentHash;
-        require(newsItems[itemHash].uploader == address(0), "Item already uploaded");
+    function uploadNewsItem(string memory _contenturl, string _category) onlyFactChecker {
+        // bytes32 itemurl = keccak256(abi.encodePacked(_contenturl, block.timestamp, msg.sender));
+        string itemurl = _contenturl;
+        require(newsItems[itemurl].uploader == address(0), "Item already uploaded");
 
         // Category can be implemented on the front-end part
         if (! findCategory(_category)) {
-            add_new_category(string _category);
+            add_new_category(_category);
         }
 
         // add a new news item
         NewsItem memory newItem;
         newItem.uploader = msg.sender;
-        newItem.contentHash = _contentHash;
+        newItem.contenturl = _contenturl;
         newItem.timestamp = block.timestamp;
         newItem.category = _category;
         newItem.totalVotes = 0;
         newItem.fakenessScore = -1; // INDICATE NOT YET DONE VOTING
 
-        newsItems[itemHash] = newItem;
+        newsItems[itemurl] = newItem;
         emit NewsItemUploaded(newItem);
     }
 
-    function start_voting(string _itemHash) {
-        voting[_itemHash] = true;
+    function start_voting(string _itemurl) {
+        voting[_itemurl] = true;
     }
-    function stop_voting(string _itemHash) {
-        voting[_itemHash] = false;
-        voting_results(newsItems[_itemHash]);
+    function stop_voting(string _itemurl) {
+        voting[_itemurl] = false;
+        voting_results(newsItems[_itemurl]);
     }
 
 
     // Function for fact-checkers to vote on an item
-    function voteOnNewsItem(string _itemHash, uint8 _vote) onlyFactChecker {
-        require(newsItems[_itemHash].uploader != address(0), "Item does not exist");
-        require(voting[_itemHash], "Voting Period not active");
+    function voteOnNewsItem(string _itemurl, uint8 _vote) onlyFactChecker {
+        require(newsItems[_itemurl].uploader != address(0), "Item does not exist");
+        require(voting[_itemurl], "Voting Period not active");
 
         // --- REQUIRE A DEPOSIT OF 'x' cryptocurrency
 
         // store the vote
-        newsItems[_itemHash].votes[msg.sender] = _vote;
-        newsItems[_itemHash].totalVotes += 1;
-        newsItems[_itemHash].voters.push(msg.sender);
+        newsItems[_itemurl].votes[msg.sender] = _vote;
+        newsItems[_itemurl].totalVotes += 1;
+        newsItems[_itemurl].voters.push(msg.sender);
     }
 
 
@@ -151,7 +153,7 @@ contract FakeNewsDApp {
         for(uint i = 0; i < news.totalVotes; i++) {
             if(news.votes[news.voters[i]] == w) {
                 good_addresses.push(news.voters[i]);
-                good_repute += reputations[news.voters[i]]
+                good_repute += reputations[news.voters[i]];
                 // WILL GET DEPOSIT BACK AND DISTRIBUTED REWARDS FROM WRONG VOTERS
             }
             else {
@@ -161,7 +163,7 @@ contract FakeNewsDApp {
             }
 
             // update weights = correct / (correct + incorrect)
-            weights[news.voters[i]] = correct_votes[news.voters[i]] / (correct_votes[news.voters[i]] + incorrect_votes[news.voters[i]])
+            weights[news.voters[i]] = correct_votes[news.voters[i]] / (correct_votes[news.voters[i]] + incorrect_votes[news.voters[i]]);
 
         }
 
@@ -175,7 +177,7 @@ contract FakeNewsDApp {
         uint avail = x * (news.totalVotes - good_addresses.length);
 
         for(uint i = 0; i < good_addresses.length; i++) {
-            uint money = x + (avail * reputations[good_addresses[i]] / good_repute)
+            uint money = x + (avail * reputations[good_addresses[i]] / good_repute);
 
             // --- GIVE 'money' TO good_addresses[i]
 
@@ -185,15 +187,17 @@ contract FakeNewsDApp {
 
 
     // Function to get overall truthfulness score of an item
-    function getTruthfulnessScore(string _itemHash) public view returns (uint256) {
-        require(newsItems[_itemHash].uploader != address(0), "Item does not exist");
-        return newsItems[_itemHash].fakenessScore;
+    function getTruthfulnessScore(string _itemurl) public view returns (uint256) {
+        require(newsItems[_itemurl].uploader != address(0), "Item does not exist");
+        return newsItems[_itemurl].fakenessScore;
     }
 
 
     // Function to add a new fact-checker
     function addFactChecker(address _factChecker, string _identity) {
+
         // --- VALIDATE IDENTITY BEFORE ADDING (off-chain)
+
         require(!findIdentity(_identity), "IDENTITY already exists");
         require(!findFactChecker(_factChecker), "ADDRESS already exists");
 
@@ -232,7 +236,7 @@ contract FakeNewsDApp {
 
     // CALL PERIODICALLY -- done off-chain
     function periodic() {
-        for (; i < factCheckers.length; i++) {
+        for (uint i = 0; i < factCheckers.length; i++) {
             address a = factCheckers[i];
             // reduce reputations
             reputations[a] = max(0, reputations[a]-5);
@@ -241,8 +245,8 @@ contract FakeNewsDApp {
 
 
     // Function to query the category of a news item
-    function getNewsItemCategory(string _itemHash) public view returns (string) {
-        return newsItems[_itemHash].category;
+    function getNewsItemCategory(string _itemurl) public view returns (string) {
+        return newsItems[_itemurl].category;
     }
     
 }
